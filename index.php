111e6cae60f98dbeb84f6fbbc2510695f97b0364
ilia4163 <60977775+ilia4163@users.noreply.github.com>
2021-06-30 15:28:52 +0300

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<? include 'config.php'; ?>
-
-<a href="https://graph.facebook.com/oauth/access_token?client_id=<?=ID?>&client_secret=<?=SECRET?>&grant_type=client_credentials">Маркер</a>
-<a href="https://graph.facebook.com/{your-user-id}/accounts?access_token={user-access-token}">k</a>
-<a href="https://www.facebook.com/v11/dialog/oauth?client_id=<?=ID?>$redirect_uri=<?=URL?>&response_type=code&scope=public_profile,email,user_location">ВОйти в fb</a>
